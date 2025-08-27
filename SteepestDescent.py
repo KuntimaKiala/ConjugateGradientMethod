fc182ff4076b2bac0271c0fca4be20ba98ab52b2
kuntimakiala <kuntimakiala@github.com>
2025-08-27 22:46:09 +0200

    Quadratic Form

--- SteepestDescent.py
+++ SteepestDescent.py
@@ -1,4 +1,12 @@
-
+import numpy as np 
 
 if "__main__" == __name__ :
-    print("\nStart")
+    print("\nSteepest Descent")
+
+    A = np.array([[3,2],[2,6]],dtype=np.float64)
+    b = np.array([[2],[-8]])
+
+    print(f"\nA\n{A}")
+    print(f"\nb\n{b}")
+    print(f"\nf(x) = xAxT - bTx + c")
+    print("\n Ax = b")
